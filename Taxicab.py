9ca74a77558b1a549d70dfb7943649a711d1a29a
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-26 12:55:41 -0700

    update

--- Taxicab.py
+++ Taxicab.py
@@ -1,30 +1,32 @@
-class Taxicab:
-    """
-    Represents a point on the Cartesian plane that can calculate the distance
-    between itself and another point
-    """
-    def __init__(self, x_coord, y_coord, odometer):
-        """Creates a Point object with the specified number of coordinates"""
-        self._x_coord = x_coord
-        self._y_coord = y_coord
-        self._odometer
+class Taxicab():
+    """"""
+
+    def __init__(self, x_coord, y_coord):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.odometer = 0
 
     def get_x_coord(self):
-        return self._x_coord
+        return self.x_coord
 
     def get_y_coord(self):
-        return self._y_coord
+        return self.y_coord
 
-    def get_odom(self):
-        return self._odometer
+    def get_odometer(self):
+        return self.odometer
 
-    def distance_to(self, other_point):
-        """returns the distance to the other point"""
-        x_diff = self._x_coord - other_point.get_x_coord()
-        y_diff = self._y_coord - other_point.get_y_coord()
-        result = (x_diff ** 2 + y_diff ** 2) ** 0.5
-        return result
+    def move_x(self, units):
+        self.x_coord += units
+        # add the absolute distance to odometer
+        self.odometer += abs(units)
+    def move_y(self, units):
+        self.y_coord += units
+        # add the absolute distance to odometer
+        self.odometer += abs(units)
 
-point_1 = Taxicab(0,4)
-point_2 = Taxicab(3,0)
-print(point_1.distance_to(point_2))
+
+cab = Taxicab(5,-8)
+cab.move_x(3)
+cab.move_y(-4)
+cab.move_x(-1)
+print(cab.odometer) # will print 8 3+4+1 = 8
