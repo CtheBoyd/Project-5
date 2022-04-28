bfae4c003ec85c0f7ecec9503336d1d9c3a11212
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-27 17:36:18 -0700

    update

--- Taxicab.py
+++ Taxicab.py
@@ -1,5 +1,11 @@
+# Author: Christopher Boyd
+# GitHub username: CtheBoyd
+# Date: 4/26/2022
+# Description: Asks user to provide five numbers and then provides the total units the taxi moved.
+#
+
 class Taxicab():
-    """"""
+    """Asks user to provide five numbers and then provides the total units the taxi moved."""
 
     def __init__(self, x_coord, y_coord):
         self.x_coord = x_coord
@@ -25,8 +31,3 @@ class Taxicab():
         self.odometer += abs(units)
 
 
-cab = Taxicab(5,-8)
-cab.move_x(3)
-cab.move_y(-4)
-cab.move_x(-1)
-print(cab.odometer) # will print 8 3+4+1 = 8
--- multiply.py
+++ multiply.py
@@ -1,5 +1,11 @@
+# Author: Christopher Boyd
+# GitHub username: CtheBoyd
+# Date: 4/26/2022
+# Description: Function multiplies two numbers through adding number 1, number 2 times.
+#
+
 def multiply(num_1, num_2):
+    """Function multiplies two numbers through adding number 1, number 2 times."""
     if num_2 == 0:
         return 0
     return num_1 + multiply(num_1, num_2 - 1)
-print(multiply(7, 4))
