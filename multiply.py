f4fc7581d980f703b9a21b95af6342b8c800dcb0
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-27 17:48:10 -0700

    update

--- multiply.py
+++ multiply.py
@@ -6,9 +6,6 @@
 
 def multiply(num_1, num_2):
     """Function multiplies two numbers through adding number 1, number 2 times."""
-    if (num_1<num_2):
-        return multiply(num_2, num_1)
-    elif(num_2!=0):
-        return (num_1 + multiply(num_1, num_2 - 1))
-    else:
+    if num_2 == 0:
         return 0
+    return num_1 + multiply(num_1, num_2 - 1)
